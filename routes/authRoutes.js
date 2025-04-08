13a0193117d5a600f06d2aa1589875220b283fb1
Anuj Kumar <144164000+anuj22045@users.noreply.github.com>
2025-04-08 15:01:54 +0530

    patient's setting interface, request prescription from patient side

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,3 +12,29 @@ router.post("/signup", authController.signupUser);
 router.post("/signin", authController.signinUser);
 
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const authController = require("../controllers/authController");
+
+// // GET Auth Page
+// router.get("/", authController.showAuthPage);
+
+// // POST Signup
+// router.post("/signup", authController.signupUser);
+
+// // POST Signin
+// router.post("/signin", authController.signinUser);
+
+// // 🔓 Logout Route
+// router.get("/logout", (req, res) => {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       console.log("Logout error:", err);
+//       return res.redirect("/patient");
+//     }
+//     res.redirect("/login");
+//   });
+// });
+
+// module.exports = router;
